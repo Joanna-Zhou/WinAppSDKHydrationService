12bb5b2aed3166f06dc62eba580b950aef80dddb
Jojo Zhou <yizzho@microsoft.com>
2023-09-12 12:55:12 -0400

    Add UI

--- HydrationApp/MainWindow.xaml.cpp
+++ HydrationApp/MainWindow.xaml.cpp
@@ -7,6 +7,11 @@
 #include "MainWindow.g.cpp"
 #endif
 
+//#include <bcrypt.h>
+//#include <cfapi.h>
+
+//#include <winrt/Microsoft.UI.Xaml.Input.h>
+
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
 
@@ -20,18 +25,55 @@ namespace winrt::HydrationApp::implementation
         InitializeComponent();
     }
 
-    int32_t MainWindow::MyProperty()
+    void MainWindow::StartButton_Click(IInspectable const&, RoutedEventArgs const&)
     {
-        throw hresult_not_implemented();
+        StartButton().Content(box_value(L"Hydrating"));
+
+        HydrateFile(FilePath().Text());
+
+        StartButton().Content(box_value(L"Hydrate file"));
     }
 
-    void MainWindow::MyProperty(int32_t /* value */)
+    void MainWindow::CancelButton_Click(IInspectable const&, RoutedEventArgs const&)
     {
-        throw hresult_not_implemented();
+        StartButton().Content(box_value(L"Cancelling"));
+
+        //CancelButton().Content(box_value(L"Clicked"));
+
+        StartButton().Content(box_value(L"Cancelling"));
     }
 
-    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    bool MainWindow::HydrateFile(std::wstring_view filePath)
     {
-        myButton().Content(box_value(L"Clicked"));
+        PrintOutputLine(L"HydrateFile: starting...");
+        winrt::handle placeholder(CreateFile(filePath.data(), 0, FILE_READ_DATA, nullptr, OPEN_EXISTING, 0, nullptr));
+
+        if (placeholder.get() != INVALID_HANDLE_VALUE)
+        {
+            LARGE_INTEGER offset;
+            offset.QuadPart = 0;
+            LARGE_INTEGER lengthOfEntireFile;
+            lengthOfEntireFile.QuadPart = -1;
+
+            //auto result = CfHydratePlaceholder(placeholder.get(), offset, lengthOfEntireFile, CF_HYDRATE_FLAG_NONE, NULL);
+
+            //placeholder.close();
+
+            //return (SUCCEEDED(result));
+        }
+
+        PrintOutputLine(L"HydrateFile: failed.");
+
+        return false;
     }
+
+    //WF::IAsyncOperation<bool> MainWindow::HydrateFileAsync(std::wstring_view filePath)
+    //{
+    //    /* Resume on a background thread here since HydrateFile()
+    //     * will block the calling thread (ie. it will cause UI to hang). */
+    //     // No need for get_strong because this is a free function
+    //    co_await winrt::resume_background();
+
+    //    co_return HydrateFile(filePath);
+    //}
 }
--- HydrationApp/MainWindow.xaml.h
+++ HydrationApp/MainWindow.xaml.h
@@ -5,16 +5,35 @@
 
 #include "MainWindow.g.h"
 
+//#include <winrt/Microsoft.UI.Xaml.Data.h>
+//namespace WUXD = winrt::Microsoft::UI::Xaml::Data;
+
 namespace winrt::HydrationApp::implementation
 {
     struct MainWindow : MainWindowT<MainWindow>
     {
         MainWindow();
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
+        winrt::hstring OutputText() { return m_outputText; };
+        void OutputText(winrt::hstring v) { m_outputText = v; m_propertyChanged(*this, Microsoft::UI::Xaml::Data::PropertyChangedEventArgs{ L"OutputText" });};
 
-        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void StartButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void CancelButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+
+        bool HydrateFile(std::wstring_view filePath);
+        //WF::IAsyncOperation<bool> HydrateFileAsync(std::wstring_view filePath);
+
+        winrt::event_token PropertyChanged(Microsoft::UI::Xaml::Data::PropertyChangedEventHandler const& handler) { return m_propertyChanged.add(handler); };
+        void PropertyChanged(winrt::event_token const& token) { m_propertyChanged.remove(token); };
+
+    private:
+        winrt::hstring m_filePath = L"C:/Users/yizzho/OneDrive - Microsoft/Pictures/TestingPhotos/Video Projects";
+        winrt::hstring m_outputText = L"Placeholder";
+
+        inline void PrintOutputLine(winrt::hstring newLine) { OutputText(m_outputText + L"\n" + newLine); }
+
+        winrt::event<Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> m_propertyChanged;
+
     };
 }
 
